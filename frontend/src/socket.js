94d7edd6e889f4db171dbc7f251514f816a8c526
eyad <eyadmosa853@gmail.com>
2025-09-08 19:03:35 +0300

    socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8000/', {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   withCredentials: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000
